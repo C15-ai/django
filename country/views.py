810b662275a155c1a7b6e24d19df8365db3576e0
momin <tursunov545@gmai.com>
2026-04-15 15:36:15 +0500

    country add

--- country/views.py
+++ country/views.py
@@ -18,14 +18,17 @@ def country_create(request):
         form = CountryForm()
 
     return render(request, 'country/country_create.html', {'form': form})
-def country_update(request,pk=None):
-    countries = Country.objects.filter(id=pk).first()
-    form = CountryForm(instance=countries,data=request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('country_list')
-    return render(request, 'country/country_update.html',{'form':form, 'countries':countries})
+def country_update(request, pk=None):
+    country = Country.objects.filter(id=pk).first()
 
+    if request.method == "POST":
+        form = CountryForm(request.POST, instance=country)
+        if form.is_valid():
+            form.save()
+            return redirect('country_list')
+    else:
+        form = CountryForm(instance=country)
+    return render(request, 'country/country_update.html', {'form': form, 'country': country})
 def country_delete(request, pk=None):
     Country.objects.filter(id=pk).delete()
     return redirect('country_list')
